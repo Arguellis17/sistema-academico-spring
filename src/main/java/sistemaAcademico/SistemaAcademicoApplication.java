b435c8c3db16c84e500cd7710a0cab564d631c3e
Arguellis17 <arguelloj866@gmail.com>
2025-03-24 16:54:01 -0500

    first commit

---
+++ src/main/java/sistemaAcademico/SistemaAcademicoApplication.java
@@ -0,0 +1,15 @@
+package sistemaAcademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcademicoApplication.class, args);
+
+		System.out.println("ola");
+	}
+
+}
