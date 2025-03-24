b249904cad1b6e5a629c8402b54b54a0d18c870c
Arguellis17 <arguelloj866@gmail.com>
2025-03-24 17:38:55 -0500

    se ha configurado el pom para que sea reconocido por intelij xd

--- src/main/java/sistemaAcademico/model/Estudiante.java
+++ src/main/java/sistemaAcademico/model/Estudiante.java
@@ -1,6 +1,8 @@
 package sistemaAcademico.model;
-import lombok.*;
+
 import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
 @Data
@@ -11,4 +13,6 @@ public class Estudiante {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    // Añade al menos un atributo más
+    private String nombre;
 }
